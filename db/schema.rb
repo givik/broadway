ce57024908b61c9b52744712c37a66214957b00c
Givi Kuchukhidze <givi.kuchukhidze@gmail.com>
2017-10-21 02:31:30 +0400

    assing category to plays

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020212639) do
+ActiveRecord::Schema.define(version: 20171020212836) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20171020212639) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
