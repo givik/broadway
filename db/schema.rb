7c7bb2ad9eb668aaca0607fe01bb56b645e649a9
Givi Kuchukhidze <givi.kuchukhidze@gmail.com>
2017-10-21 01:36:01 +0400

    categories model and assign plays to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020210344) do
+ActiveRecord::Schema.define(version: 20171020212639) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "plays", force: :cascade do |t|
     t.string "title"
