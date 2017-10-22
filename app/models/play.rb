26dcbc072213ecc56a148ffb5b991e24695bc781
Givi Kuchukhidze <givi.kuchukhidze@gmail.com>
2017-10-23 01:56:54 +0400

    review model, migrations and model associations

--- app/models/play.rb
+++ app/models/play.rb
@@ -1,7 +1,8 @@
 class Play < ApplicationRecord
   belongs_to :user
   belongs_to :category
-
+  has_many :reviews
+  
   has_attached_file :play_img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :play_img, content_type: /\Aimage\/.*\z/
 end
